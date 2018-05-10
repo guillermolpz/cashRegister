b7db99dacb9e56f9cf5a090b06b3a8a212a69cf1
Josue123 <josueflores.05@gmail.com>
2018-05-10 13:38:39 -0500

    Add other scope API SHopify

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -2,7 +2,7 @@ ShopifyApp.configure do |config|
   config.application_name = "My Shopify App"
   config.api_key = "c6014856dc7054ed2d79814277b264d9"
   config.secret = "55d3fbb5ceeb236ea9806df98180ec42"
-  config.scope = "read_orders, read_products"
+  config.scope = "read_products, write_products, read_product_listings, read_customers, write_customers, read_orders, write_orders, read_draft_orders, write_draft_orders, read_shipping, write_shipping, read_checkouts, write_checkouts, read_price_rules, write_price_rules"
   config.embedded_app = true
   config.after_authenticate_job = false
   config.session_repository = Shop
