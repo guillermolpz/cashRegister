5a84067fb2dea8a88c340c55f1f3c139879e8a69
Josue123 <josueflores.05@gmail.com>
2018-05-10 21:14:20 -0500

    Add view, controller and assets

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,19 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
-    @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
-    @webhooks = ShopifyAPI::Webhook.find(:all)
+    @products = ShopifyAPI::Product.find(:all, :params => {:limit => 10})
   end
+
+  def cart
+    @products = ShopifyAPI::Product.find(:all, :params => {:limit => 10})
+  end
+
+  def discounts
+  end
+
+  def loyalty
+  end
+
+  def customer
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   root :to => 'home#index'
   mount ShopifyApp::Engine, at: '/'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'cart' => 'home#cart'
+  get 'random-product' => 'home#cart'
+  get 'discounts' => 'home#discounts'
+  get 'loyalty' => 'home#loyalty'
+  get 'customer' => 'home#customer'
 end
