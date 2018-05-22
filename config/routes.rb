c9074fecae348aa98c67ec32804c41733375e7f7
Josue123 <josueflores.05@gmail.com>
2018-05-22 09:21:25 -0500

    Initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
+  get 'order_items/create'
+  get 'order_items/update'
+  get 'order_items/destroy'
+  get 'carts/show'
+  get 'products/index'
   devise_for :users
   #devise_for :users, controllers: { sessions: 'users/sessions' }
+
+  resources :products, only: [:index]
+  resource :cart, only: [:show]
+  resources :order_items, only: [:create, :update, :destroy]
+
   root :to => 'home#index'
   mount ShopifyApp::Engine, at: '/'
 
