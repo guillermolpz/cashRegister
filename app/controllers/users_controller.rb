00912dc1b8431e8b82d3dbd325e71a9d441be277
Josue123 <josueflores.05@gmail.com>
2018-05-25 13:36:54 -0500

    CRUD operation user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   load_and_authorize_resource
   before_action :authenticate_user!
 
-  before_action :set_user, only: [:show, :edit, :update] # probably want to keep using this
+  before_action :set_user, only: [:show, :new, :edit, :create, :update] # probably want to keep using this
 
   # GET /users
   # GET /users.json
@@ -16,17 +16,39 @@ class UsersController < ApplicationController
 
   end
 
+  # GET /users/new
+  def new
+    @user = User.new
+  end
+
   # GET /users/1/edit
   def edit
 
   end
+
+  # POST /users
+  # POST /users.json
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'Usuario creado correctamente.' }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
 
   # # PATCH/PUT /users/1
   # # PATCH/PUT /users/1.json
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.html { redirect_to @user, notice: 'Usuario actualizado correctamente' }
         format.json { render :show, status: :ok, location: @user }
       else
         format.html { render :edit }
@@ -35,15 +57,25 @@ class UsersController < ApplicationController
     end
   end
 
+  # DELETE /users/1
+  # DELETE /users/1.json
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to users_url, notice: 'Usuario eliminado correctamente.' }
+      format.json { head :no_content }
+    end
+  end
+
   private
   # Use callbacks to share common setup or constraints between actions.
   def set_user
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
   end
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def user_params
-    params.require(:user).permit(:roles_mask, :name)
+    params.require(:user).permit(:name, :lastName, :username, :email, :password, :password_confirmation, roles: [])
   end
 
 
